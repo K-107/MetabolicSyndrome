36a05a1be8f82b2aebf2f02b38a46c059c544ba2
HyeJin Baek <45028735+hyejinbaek@users.noreply.github.com>
2022-06-08 16:09:55 +0900

    Delete Srcs/MetabolicSyndrome/HJH directory

--- Srcs/MetabolicSyndrome/HJH/data_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy import stats
-
-# sample data test
-sample_data = pd.read_csv('/Data1/Srcs/MetabolicSyndrome/HJH/data/sample_submission.csv')
-print(sample_data)
