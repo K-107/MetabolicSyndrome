0e2090f1d353433204f8fd51c3588663c0457b94
hyejinbaek <bhj4208@gmail.com>
2022-06-08 06:19:22 +0000

    ai contest data preprocessing

---
+++ Srcs/MetabolicSyndrome/HJH/data_preprocessing.py
@@ -0,0 +1,9 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+# sample data test
+sample_data = pd.read_csv('/Data1/Srcs/MetabolicSyndrome/HJH/data/sample_submission.csv')
+print(sample_data)
