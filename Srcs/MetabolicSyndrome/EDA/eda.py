550392a3f7185222021e2995fc0995635a484658
hyejinbaek <bhj4208@gmail.com>
2022-07-07 08:10:47 +0000

    add xgboost

--- Srcs/MetabolicSyndrome/EDA/eda.py
+++ Srcs/MetabolicSyndrome/EDA/eda.py
@@ -14,7 +14,7 @@ myungi = pd.read_excel('/Data2/Dataset/Myongji/Myongji_2019-2021.xlsx', sheet_na
 
 # koges dataset
 def koges1():
-	ko
+	print(" ")
 
 # myungi dataset
 def myungi1():
@@ -30,16 +30,45 @@ def myungi1():
                        '허리둘레(단위:cm)':'WAIST', '체질량지수(BMI)':'BMI', '혈압(최고)':'SBP'}, inplace=True)
 	# drink null change
 	myungi_1.DRINK.replace(np.nan,'0', inplace=True)
-
 	# nan column delete
 	myungi_1_2 = myungi_1.drop(['Unnamed: 0','Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18'],axis=1)
 	#print(myungi_1_2)
-
-	# pearson
-	corr = myungi_1_2.corr()
-	#print(corr)
 	
+	# change type = 'DRINK' column
+	myungi_1_2['DRINK'] = myungi_1_2['DRINK'].astype(float)
+	
+	# xgboost
+	from xgboost import XGBClassifier
+	from xgboost import plot_importance
+	import pandas as pd
+	import numpy as np
+	from sklearn.datasets import load_breast_cancer
+	from sklearn.model_selection import train_test_split
+	import warnings
+	warnings.filterwarnings('ignore')
+	from sklearn.preprocessing import LabelEncoder
 
+	X = myungi_1_2.drop(['SEX'], axis = 1)
+	y = myungi_1_2['SEX']
+	encoder = LabelEncoder()
+	encoder.fit(y)
+	labels = encoder.transform(y)
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 156)
+
+	evals = [X_test, y_test]
+	xgb_wrapper = XGBClassifier(n_estimators=400, learning_rate = 0.1, max_depth=3)
+
+	xgb_wrapper.fit(X_train, y_train)
+
+	w_pred = xgb_wrapper.predict(X_test)
+
+	w_pred = xgb_wrapper.predict_proba(X_test)[:, 1]
+	
+	model.fit(X_train, y_train, eval_set = [(X_train, y_train), (X_valid, y_valid)], 
+												early_stopping_rounds = 100, verbose = False)
+
+	
 
 if __name__ == '__main__':
 	myungi1()
